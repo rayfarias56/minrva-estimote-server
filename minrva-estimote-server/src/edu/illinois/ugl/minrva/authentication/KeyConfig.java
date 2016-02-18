e773b99baad3e6c4c780c3b4689f170472d24c49
Raymond Farias <rayfarias56@gmail.com>
2016-02-18 15:07:28 -0600

    Removing key generator

--- minrva-estimote-server/src/edu/illinois/ugl/minrva/authentication/KeyConfig.java
+++ minrva-estimote-server/src/edu/illinois/ugl/minrva/authentication/KeyConfig.java
@@ -4,8 +4,6 @@ import javax.crypto.SecretKey;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import io.jsonwebtoken.impl.crypto.MacProvider;
-
 public class KeyConfig implements ServletContextListener {
 	private final String ENVIRONMENT_KEY_NAME = "minrva_signing_key";
 	private static SecretKey key;
