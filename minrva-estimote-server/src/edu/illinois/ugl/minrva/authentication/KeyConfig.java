57c79880a2f3e50fbafa389623443db65ecba1fe
Raymond Farias <rayfarias56@gmail.com>
2016-02-18 15:06:43 -0600

    Removing Debug Print

--- minrva-estimote-server/src/edu/illinois/ugl/minrva/authentication/KeyConfig.java
+++ minrva-estimote-server/src/edu/illinois/ugl/minrva/authentication/KeyConfig.java
@@ -4,6 +4,8 @@ import javax.crypto.SecretKey;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import io.jsonwebtoken.impl.crypto.MacProvider;
+
 public class KeyConfig implements ServletContextListener {
 	private final String ENVIRONMENT_KEY_NAME = "minrva_signing_key";
 	private static SecretKey key;
@@ -20,7 +22,6 @@ public class KeyConfig implements ServletContextListener {
 							+ "It may not be set.");
 		} else {
 			key = KeyCodec.decodeKey(encodedKey);
-			System.out.println(key);
 		}
 		
 	}
